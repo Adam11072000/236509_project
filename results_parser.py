2ca8b39eff962fd377c0e924a03351a881e5586c
Adam Ghadban <ghadbanadam@Adams-MacBook-Pro-2.local>
2023-07-31 14:31:51 +0300

    consider changing the uniform and gaussian distribution to have more meaning

--- results_parser.py
+++ results_parser.py
@@ -2,62 +2,93 @@ import json
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import copy
 
 OUTPUT_FIGS_DIR = "./output_figures"
 
-def plot_results(data: dict, name: str, sorting_key):
-    plt.figure(figsize=(10,6))
+def plot_results(data: dict, name: str):
+    fig, axs = plt.subplots(2, figsize=(10,10))
+    fig.suptitle(f"{generic_fault_model}_{fault_target}")
+    for fault_place_in_nn, distributions in data.items():
+        for ax, (distribution, workers_data) in zip(axs, distributions.items()):
+            avg_values = [np.mean(worker_data["acc_per_iteration"]) for worker_data in workers_data]
+            x = range(len(avg_values))
+            ax.plot(x, avg_values, label=fault_place_in_nn)
+            ax.set_title(distribution)
+    for ax in axs:
+        ax.legend()
+        ax.grid(True)
+    # Show the figure
+    plt.savefig(f"./output_figures/{name}")
 
-    x = range(1, len(data) + 1)
-    data = sorted(data, key=lambda x: x[sorting_key])
-    max_data = []
-    min_data = []
-    avg = []
-    for fault_model in data:
-        max_data.append(max(fault_model["acc_per_iteration"]))
-        min_data.append(min(fault_model["acc_per_iteration"]))
-        avg.append(np.mean(fault_model["acc_per_iteration"]))
-
-    plt.plot(x, max_data, label='max over fault models')
-    plt.plot(x, min_data, label='min over fault models')
-    plt.plot(x, avg, label='avg over fault models')
-
-    plt.title("%s, original accuracy: %s" % (name, data[0]["original_acc"]))
-    plt.xlabel("Fault")
-    plt.ylabel("Accuracy")
-
-    plt.grid()
-    plt.legend()
-
-    name = name.replace(" ", "_")
-    if not os.path.isdir(OUTPUT_FIGS_DIR):
-        os.mkdir(OUTPUT_FIGS_DIR)
-    plt.savefig(os.path.join(f"{OUTPUT_FIGS_DIR}/{name}.png"))
-
-
-def parse_results(target_dir: str, target_key):
-    # each dir has the main metric, but also faults locations are distributed via 2 different distributions, uniform and gaussian
-    faults = {}
-    for file in os.listdir(target_dir):
-        with open(os.path.join(target_dir, file), "r") as file_descriptor:
-            worker_data = json.load(file_descriptor)
-            file_descriptor.close()
-        fault_distribution = worker_data[list(worker_data.keys())[0]]["fault_distribution"]
-        if not fault_distribution in faults:
-            faults[fault_distribution] = []
-        faults[fault_distribution].append(worker_data[list(worker_data.keys())[0]])
-    
-    plot_results(faults["uniform"], f"Uniform distrubuted faults over {target_dir}", target_key)
-    plot_results(faults["gaussian"], f"Gaussian distrubuted faults over {target_dir}", target_key)
-
+dirs = [
+    "distribution_bit_flips_20",
+    "distribution_bit_flips_100",
+    "distribution_bit_flips_60",
+    "distribution_faults",
+    "fault_number",
+    "fault_number_randomized_bits",
+    "high_bits",
+    "middle_bits",
+    "low_bits"
+]
 
 
 if __name__ == "__main__":
-    for entry in os.listdir("."):
-        if (entry.find("_bias") != -1 or entry.find("_memory") != -1 or entry.find("_weight") != -1) and os.path.isdir(entry):
-            if entry.find("bit_flips") != -1:
-                target_key = "num_bits_to_flip"
-            else:
-                target_key = "num_faults"
-            parse_results(entry, target_key)
+    distributions = {
+        "uniform":[],
+        "gaussian":[]
+    }
+    fault_place = {
+        "early": copy.deepcopy(distributions),
+        "early_middle": copy.deepcopy(distributions),
+        "late_middle": copy.deepcopy(distributions),
+        "end": copy.deepcopy(distributions)
+    }
+    fault_types = {
+        "weight": copy.deepcopy(fault_place),
+        "bias": copy.deepcopy(fault_place),
+        "memory": copy.deepcopy(fault_place)
+    }
+    place_mappings = {
+        "bias": {
+            "early": "bn1",
+            "early_middle": "layer3.0.bn1",
+            "late_middle": "layer3.1.bn1",
+            "end": "layer4.1.bn2"
+        },
+        "memory": {
+            "early": "conv1",
+            "early_middle": "layer3.0.conv2",
+            "late_middle": "layer4.0.conv1",
+            "end": "layer4.1.conv2"
+        },
+        "weight": {
+            "early": "conv1",
+            "early_middle": "layer3.0.conv2",
+            "late_middle": "layer4.0.conv1",
+            "end": "layer4.1.conv2"
+        }
+    }
+    faults = {}
+    for dir_type in dirs:
+        faults[dir_type] = copy.deepcopy(fault_types)
+    for generic_fault_model in faults:
+        for fault_target in faults[generic_fault_model]:
+            for fault_place_in_nn in faults[generic_fault_model][fault_target]:
+                fault_model = generic_fault_model + "_" + fault_target + "_" + place_mappings[fault_target][fault_place_in_nn]
+                for data in os.listdir(f"./{fault_model}"):
+                    with open(os.path.join(fault_model, data), "r") as file_descriptor:
+                        worker_data = json.load(file_descriptor)
+                        file_descriptor.close()
+                    if "fault_distribution" in worker_data:
+                        fault_distribution = worker_data["fault_distribution"]
+                    else:
+                        fault_distribution = worker_data[list(worker_data.keys())[0]]["fault_distribution"]
+                        worker_data = worker_data[list(worker_data.keys())[0]]
+                    faults[generic_fault_model][fault_target][fault_place_in_nn][fault_distribution].append(worker_data)
     
+    for generic_fault_model in faults:
+        for fault_target in faults[generic_fault_model]:
+            fault_model = generic_fault_model + "_" + fault_target
+            plot_results(faults[generic_fault_model][fault_target], fault_model)
