85c4cc983337221fa0bee3d9a256a79d9c4f8f78
Adam Ghadban <ghadbanadam@Adams-MacBook-Pro-2.local>
2023-08-07 11:32:54 +0300

    too much compute, removed a few fault models

--- runner.sh
+++ runner.sh
@@ -16,7 +16,7 @@ usage() {
 FAULT_TARGET=""
 FAULT_MODELS=()
 LAYER_NAMES=()
-FAULT_DISTRIB=( "uniform" "gaussian" )
+DISTRIB="gaussian"
 
 # Parse command line flags
 while getopts "ht:m:l:" opt; do
@@ -53,68 +53,59 @@ fi
 
 fault_number_models(){
     layer_name=$1
-    distrib=$2
     # fixed bits
     python3 run_injector.py -n 50 -i 100 --target_bits 19 20 21 22 23 --fault_target $FAULT_TARGET\
-        --output_dir fault_number_$FAULT_TARGET"_$layer_name""_$distrib" --layer_name $layer_name --distribution $distrib
+        --output_dir fault_number_$FAULT_TARGET"_$layer_name" --layer_name $layer_name --distribution $DISTRIB
 
     #randomized bits
     python3 run_injector.py -n 50 -i 100 --fault_target $FAULT_TARGET\
-        --output_dir fault_number_randomized_bits_$FAULT_TARGET"_$layer_name""_$distrib" --layer_name $layer_name --distribution $distrib
+        --output_dir fault_number_randomized_bits_$FAULT_TARGET"_$layer_name" --layer_name $layer_name --distribution $DISTRIB
 }
 
 distribution_bit_flips_models(){
     layer_name=$1
-    distrib=$2
     python3 run_injector.py -n 50 -i 100 --fault_target $FAULT_TARGET\
-        --output_dir distribution_bit_flips_20_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
-
-    python3 run_injector.py -n 50 -i 100 --fault_target $FAULT_TARGET\
-        --output_dir distribution_bit_flips_60_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 60 --layer_name $layer_name --distribution $distrib
-
-    python3 run_injector.py -n 50 -i 100 --fault_target $FAULT_TARGET\
-        --output_dir distribution_bit_flips_100_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 100 --layer_name $layer_name --distribution $distrib
+        --output_dir distribution_bit_flips_20_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
 }
 
 distribution_faults_models(){
     layer_name=$1 
-    distrib=$2
     python3 run_injector.py -n 50 -i 100 --target_bits 19 20 21 22 23 --fault_target $FAULT_TARGET\
-        --output_dir distribution_faults_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir distribution_faults_$FAULT_TARGET"_$layer_name""_gaussian" --num_faults 20 --layer_name $layer_name --distribution gaussian
+
+    python3 run_injector.py -n 50 -i 100 --target_bits 19 20 21 22 23 --fault_target $FAULT_TARGET\
+        --output_dir distribution_faults_$FAULT_TARGET"_$layer_name""_uniform" --num_faults 20 --layer_name $layer_name --distribution uniform
 }
 
 bits_granularity_models(){
     layer_name=$1 
-    distrib=$2
 
     # mantissa bits
     python3 run_injector.py -n 50 -i 100 --target_bits 10 11 12 13 14 --fault_target $FAULT_TARGET\
-        --output_dir middle_bits_mantissa_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir middle_bits_mantissa_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
     python3 run_injector.py -n 50 -i 100 --target_bits 0 1 2 3 4 5 --fault_target $FAULT_TARGET\
-        --output_dir low_bits_mantissa_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir low_bits_mantissa_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
     python3 run_injector.py -n 50 -i 100 --target_bits 19 20 21 22 23 --fault_target $FAULT_TARGET\
-        --output_dir high_bits_mantissa_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir high_bits_mantissa_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
 
     # exponent bits
     python3 run_injector.py -n 50 -i 100 --target_bits 29 30 --fault_target $FAULT_TARGET\
-        --output_dir high_bits_exponent_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir high_bits_exponent_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
     python3 run_injector.py -n 50 -i 100 --target_bits 24 25 --fault_target $FAULT_TARGET\
-        --output_dir low_bits_exponent_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir low_bits_exponent_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
     python3 run_injector.py -n 50 -i 100 --target_bits 27 28 --fault_target $FAULT_TARGET\
-        --output_dir middle_bits_exponent_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir middle_bits_exponent_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
 
     # sign bit
     python3 run_injector.py -n 50 -i 100 --target_bits 31 --fault_target $FAULT_TARGET\
-        --output_dir sign_bit_$FAULT_TARGET"_$layer_name""_$distrib" --num_faults 20 --layer_name $layer_name --distribution $distrib
+        --output_dir sign_bit_$FAULT_TARGET"_$layer_name" --num_faults 20 --layer_name $layer_name --distribution $DISTRIB
 }
 
 
 for MODEL in "${FAULT_MODELS[@]}"; do
   if declare -f $MODEL > /dev/null; then
     for layer_name in "${LAYER_NAMES[@]}"; do
-      for distrib in "${FAULT_DISTRIB[@]}"; do
-        eval "$MODEL $layer_name $distrib"
-      done
+      eval "$MODEL $layer_name"
     done
   else
     echo "Invalid fault model: $MODEL. Choose from fault_number_models, distribution_bit_flips_models,distribution_faults_models or bits_granularity_models."
