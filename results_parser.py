25131db6952ff08acfdfbd3c324418b585e8a9d4
Adam Ghadban <ghadbanadam@Adams-MacBook-Pro-2.local>
2023-08-08 14:11:38 +0300

    updated parser to parse according to fault model

--- results_parser.py
+++ results_parser.py
@@ -6,47 +6,62 @@ import copy
 
 OUTPUT_FIGS_DIR = "./output_figures"
 
-def plot_results(data: dict, name: str):
+def plot_distribution_fault_models(data: dict, fault_model: str, distribution_target: str, sort_key: str):
     fig, axs = plt.subplots(2, figsize=(10,10))
-    fig.suptitle(f"{generic_fault_model}_{fault_target}")
-    for fault_place_in_nn, distributions in data.items():
-        for ax, (distribution, workers_data) in zip(axs, distributions.items()):
-            sorted_list_of_dicts = sorted(workers_data, key=lambda x: x['num_faults'])
+    fig.suptitle(fault_model)
+    for ax, bit_flips_distrib in zip(axs, ("gaussian", "uniform")):
+        for fault_place_in_nn, workers_data in data.items():
+            workers = [worker for worker in workers_data if worker[distribution_target] == bit_flips_distrib]
+            sorted_list_of_dicts = sorted(workers, key=lambda x: x[sort_key])
             avg_values = [np.mean(worker_data["acc_per_iteration"]) for worker_data in sorted_list_of_dicts]
-            x = range(len(avg_values))
+            x = range(len(sorted_list_of_dicts))
             ax.plot(x, avg_values, label=fault_place_in_nn)
-            ax.set_title(distribution)
+            ax.set_title(bit_flips_distrib)
+
     for ax in axs:
         ax.legend()
         ax.grid(True)
-        ax.set_xlabel("Faults")
+        ax.set_xlabel("Fault")
         ax.set_ylabel("Accuracy")
+    plt.savefig(f"./output_figures/{fault_model}")
+
+
+def plot_results(data: dict, name: str):
+    fig, ax = plt.subplots(1, figsize=(10,10))
+    fig.suptitle(f"{generic_fault_model}_{fault_target}")
+    for fault_place_in_nn, workers_data in data.items():
+        sorted_list_of_dicts = sorted(workers_data, key=lambda x: x["num_faults"])
+        avg_values = [np.mean(worker_data["acc_per_iteration"]) for worker_data in sorted_list_of_dicts]
+        x = range(len(avg_values))
+        ax.plot(x, avg_values, label=fault_place_in_nn)
+        ax.set_title(distribution)
+    ax.legend()
+    ax.grid(True)
+    ax.set_xlabel("Faults")
+    ax.set_ylabel("Accuracy")
     # Show the figure
     plt.savefig(f"./output_figures/{name}")
 
 dirs = [
     "distribution_bit_flips_20",
-    "distribution_bit_flips_100",
-    "distribution_bit_flips_60",
     "distribution_faults",
     "fault_number",
-    "fault_number_randomized_bits",
-    "high_bits",
-    "middle_bits",
-    "low_bits"
+    #"fault_number_randomized_bits",
+    "high_bits_exponent",
+    "high_bits_mantissa",
+    "middle_bits_exponent",
+    "middle_bits_mantissa",
+    "low_bits_exponent",
+    "low_bits_manstissa",
 ]
 
 
 if __name__ == "__main__":
-    distributions = {
-        "uniform":[],
-        "gaussian":[]
-    }
     fault_place = {
-        "early": copy.deepcopy(distributions),
-        "early_middle": copy.deepcopy(distributions),
-        "late_middle": copy.deepcopy(distributions),
-        "end": copy.deepcopy(distributions)
+        "early": [],
+        "early_middle": [],
+        "late_middle": [],
+        "end": []
     }
     fault_types = {
         "weight": copy.deepcopy(fault_place),
@@ -56,20 +71,20 @@ if __name__ == "__main__":
     place_mappings = {
         "bias": {
             "early": "bn1",
-            "early_middle": "layer3.0.bn1",
+            "early_middle": "layer2.1.bn1",
             "late_middle": "layer3.1.bn1",
-            "end": "layer4.1.bn2"
+            "end": "layer4.1.bn1"
         },
         "memory": {
             "early": "conv1",
-            "early_middle": "layer3.0.conv2",
-            "late_middle": "layer4.0.conv1",
+            "early_middle": "layer2.1.conv2",
+            "late_middle": "layer3.1.conv2",
             "end": "layer4.1.conv2"
         },
         "weight": {
             "early": "conv1",
-            "early_middle": "layer3.0.conv2",
-            "late_middle": "layer4.0.conv1",
+            "early_middle": "layer2.1.conv2",
+            "late_middle": "layer3.1.conv2",
             "end": "layer4.1.conv2"
         }
     }
@@ -79,19 +94,24 @@ if __name__ == "__main__":
     for generic_fault_model in faults:
         for fault_target in faults[generic_fault_model]:
             for fault_place_in_nn in faults[generic_fault_model][fault_target]:
-                fault_model = generic_fault_model + "_" + fault_target + "_" + place_mappings[fault_target][fault_place_in_nn]
-                for data in os.listdir(f"./{fault_model}"):
-                    with open(os.path.join(fault_model, data), "r") as file_descriptor:
-                        worker_data = json.load(file_descriptor)
-                        file_descriptor.close()
-                    if "fault_distribution" in worker_data:
-                        fault_distribution = worker_data["fault_distribution"]
-                    else:
-                        fault_distribution = worker_data[list(worker_data.keys())[0]]["fault_distribution"]
-                        worker_data = worker_data[list(worker_data.keys())[0]]
-                    faults[generic_fault_model][fault_target][fault_place_in_nn][fault_distribution].append(worker_data)
+                for distribution in ("gaussian", "uniform"):
+                    fault_model = generic_fault_model + "_" + fault_target + "_" + place_mappings[fault_target][fault_place_in_nn]
+                    fault_model += f"_{distribution}"
+                    try:
+                        for data in os.listdir(f"./{fault_model}"):
+                            with open(os.path.join(fault_model, data), "r") as file_descriptor:
+                                worker_data = json.load(file_descriptor)
+                                file_descriptor.close()        
+                            faults[generic_fault_model][fault_target][fault_place_in_nn].append(worker_data)
+                    except:
+                        pass
     
     for generic_fault_model in faults:
         for fault_target in faults[generic_fault_model]:
             fault_model = generic_fault_model + "_" + fault_target
-            plot_results(faults[generic_fault_model][fault_target], fault_model)
+            if generic_fault_model == "distribution_bit_flips_20":
+                plot_distribution_fault_models(faults[generic_fault_model][fault_target], fault_model, "fault_distrubution_per_target", "num_bits_to_flip")
+            elif generic_fault_model == "distribution_faults":
+                plot_distribution_fault_models(faults[generic_fault_model][fault_target], fault_model, "fault_distribution", "num_faults")
+            else:
+                plot_results(faults[generic_fault_model][fault_target], fault_model)
